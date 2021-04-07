39b1bfb13abe67ef76bd2b0661efeccd39aa8803
sho <yoshioka0606@outlook.jp>
2021-04-07 16:21:08 +0900

    youtube_random_2

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,7 +4,7 @@ require 'active_support/all'
 
 GOOGLE_API_KEY="AIzaSyCdBixnc_GiqWpyujqvrP1nrAhKGWg4Jnc"
 
-def find_videos(keywords, after: 1.months.ago, before: Time.now)
+def self.find_videos(keywords, after: 1.months.ago, before: Time.now)
   keywords.each do |keyword|
   service = Google::Apis::YoutubeV3::YouTubeService.new
   service.key = GOOGLE_API_KEY
