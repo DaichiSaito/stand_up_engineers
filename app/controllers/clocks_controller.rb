46d6dbdca6b4e962025b0b04e386700ddd4162b8
sho <yoshioka0606@outlook.jp>
2021-04-15 10:23:33 +0900

    add_frash_messages

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -1,16 +1,20 @@
 class ClocksController < ApplicationController
     def create
-        current_user =  User.find(session[:user_id])
+        user =  User.find(session[:user_id])
         clock = Clock.new(clock_params)
-        clock.user_id = current_user.id
+        clock.user_id = user.id
         clock.start_time = Time.now
-        clock.save
-        redirect_to root_path
+        
+         if clock.save
+           redirect_to root_path, notice: "アプリを起動しました!"
+         else
+           redirect_to root_path, danger: "全ての項目を入力してください!"
+         end
     end
 
     private
 
-    def clock_params
-        params.require(:clock).permit(:set_time, :break_time, :category)
-    end
+      def clock_params
+          params.require(:clock).permit(:set_time, :break_time, :category)
+      end
 end
