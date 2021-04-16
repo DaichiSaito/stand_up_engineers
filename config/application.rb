635d58868fd302ab8704ea9aa29c1f1365028b02
sho <yoshioka0606@outlook.jp>
2021-04-16 09:43:04 +0900

    change_time_zone

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module StandUpEngineer
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
